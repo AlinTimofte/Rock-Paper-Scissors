02921a4e074d5f6644cf1b8e69c2f4e122063b51
AlinTimofte <alintimofte134@gmail.com>
2023-07-26 09:40:53 +0300

    add function getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Hello world");
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 10) % 3;
+    if(choice === 0)
+        return "Rock";
+    if(choice === 1)
+        return "Paper";
+    return "Scissors"; 
+}
+
+alert(getComputerChoice());
