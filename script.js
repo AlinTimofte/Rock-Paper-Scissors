425a75e3d546a79628d1774ea4e0e88b2144c79c
AlinTimofte <alintimofte134@gmail.com>
2023-07-26 09:53:40 +0300

    add playRound function

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 10) % 3;
     if(choice === 0)
-        return "Rock";
+        return "rock";
     if(choice === 1)
-        return "Paper";
-    return "Scissors"; 
+        return "paper";
+    return "scissors"; 
 }
 
-alert(getComputerChoice());
+function playRound (playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection)
+        return "It is a tie!";
+    switch(playerSelection)
+    {
+        case "rock":
+        if(computerSelection === "paper")
+            return "You lose! Paper beats rock!";
+        else
+            return "You win! Rock beats scissors!";
+        break;
+
+        case "paper":
+        if(computerSelection === "scissors")
+            return "You lose! Scissors beat paper!";
+        else
+            return "You win! Paper beats rock!";
+        break;
+
+        case "scissors":
+        if(computerSelection === "rock")
+            return "You lose! Rock beats scissors!";
+        else
+            return "You win! Scissors beat paper!";
+        break;
+
+        default:
+            return "Error! Wrong input";
+    }
+
+}
+
+alert(playRound("rock", getComputerChoice()));
